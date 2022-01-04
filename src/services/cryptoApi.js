1af4a4a07b728c70c94f3a75005bdf9c23f2c5b8
unknown <xavier.accerani@gmail.com>
2022-01-04 17:59:46 +0100

    recupération des données des coins avec axios

--- src/services/cryptoApi.js
+++ src/services/cryptoApi.js
@@ -1,23 +1,56 @@
-import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
+// import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 
-const cryptoApiHeaders = {
+// const cryptoApiHeaders = {
+//     'x-rapidapi-host': 'coinranking1.p.rapidapi.com',
+//     'x-rapidapi-key': 'ee275a60bcmsh02e88951a12e383p1ee182jsn0a5b1b218f2c'
+// }
+
+// const baseUrl = 'https://coinranking1.p.rapidapi.com/'
+
+// const createRequest = (url) => ({url, headers: cryptoApiHeaders})
+
+// export const cryptoApi = createApi({
+//     reducerPath: 'cryptoApi',
+//     baseQuery: fetchBaseQuery({ baseUrl }),
+//     endpoints: (builder) => ({
+//         getCryptos: builder.query({
+//             query: () => {createRequest('/exchanges')}
+//         })
+
+//     })
+// })
+// export const { useGetCryptosQuery } = cryptoApi;
+// console.log('usegetcryptoquery : ',useGetCryptosQuery )
+
+
+
+
+var axios = require("axios").default;
+
+var options = {
+  method: 'GET',
+  url: 'https://coinranking1.p.rapidapi.com/coins',
+//   params: {
+//     referenceCurrencyUuid: 'yhjMzLPhuIDl',
+//     timePeriod: '24h',
+//     symbols: 'BTC',
+//     uuids: 'Qwsogvtv82FCd',
+//     tiers: '1',
+//     tags: 'defi',
+//     orderBy: 'marketCap',
+//     search: 'Bitco',
+//     orderDirection: 'desc',
+//     limit: '50',
+//     offset: '0'
+//   },
+  headers: {
     'x-rapidapi-host': 'coinranking1.p.rapidapi.com',
     'x-rapidapi-key': 'ee275a60bcmsh02e88951a12e383p1ee182jsn0a5b1b218f2c'
-}
+  }
+};
 
-const baseUrl = 'https://coinranking1.p.rapidapi.com/'
-
-const createRequest = (url) => ({url, headers: cryptoApiHeaders})
-
-export const cryptoApi = createApi({
-    reducerPath: 'cryptoApi',
-    baseQuery: fetchBaseQuery({ baseUrl }),
-    endpoints: (builder) => ({
-        getCryptos: builder.query({
-            query: () => {createRequest('/coins')}
-        })
-
-    })
-})
-
-export const { useGetCryptosQuery } = cryptoApi;
+axios.request(options).then(function (response) {
+	console.log("LA REPONSE :",response.data);
+}).catch(function (error) {
+	console.error("erreur au niveau du request ",error);
+});
