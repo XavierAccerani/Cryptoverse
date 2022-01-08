eaf3633672d1031622bebe359b81ef6b03ec22cb
unknown <xavier.accerani@gmail.com>
2022-01-08 21:46:54 +0100

    fix api bug with toolkit

--- src/components/Cryptocurrencies.jsx
+++ src/components/Cryptocurrencies.jsx
@@ -1,6 +1,20 @@
-import React from 'react'
+import React, { useState } from 'react'
+import millify from 'millify';
+import { Link } from 'react-router-dom';
+import { Card, Row, Col, Input} from 'antd';
+
+
+import { useGetCryptosQuery } from '../services/cryptoApi';
+
 
 function Cryptocurrencies() {
+    const {data: cryptosList, isFetching} = useGetCryptosQuery(10);
+    const [ cryptos, setCryptos ] = useState(cryptosList?.data?.coins);
+
+// 58 01
+
+    console.log("cryptos : ", cryptos) 
+
     return (
         <div>
             Cryptocurrencies
--- src/services/cryptoApi.js
+++ src/services/cryptoApi.js
@@ -1,56 +1,69 @@
-// import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 
-// const cryptoApiHeaders = {
-//     'x-rapidapi-host': 'coinranking1.p.rapidapi.com',
-//     'x-rapidapi-key': 'ee275a60bcmsh02e88951a12e383p1ee182jsn0a5b1b218f2c'
-// }
+const cryptoApiHeaders = {
+    'x-rapidapi-host': 'coinranking1.p.rapidapi.com',
+    'x-rapidapi-key': 'ee275a60bcmsh02e88951a12e383p1ee182jsn0a5b1b218f2c'
+}
 
-// const baseUrl = 'https://coinranking1.p.rapidapi.com/'
+const baseUrl = 'https://coinranking1.p.rapidapi.com/'
 
-// const createRequest = (url) => ({url, headers: cryptoApiHeaders})
+const createRequest = (url) => ({ url, headers: cryptoApiHeaders });  
+
+
 
 // export const cryptoApi = createApi({
 //     reducerPath: 'cryptoApi',
 //     baseQuery: fetchBaseQuery({ baseUrl }),
 //     endpoints: (builder) => ({
 //         getCryptos: builder.query({
-//             query: () => {createRequest('/exchanges')}
+//             query: () => {createRequest('/coins')}
 //         })
 
-//     })
-// })
-// export const { useGetCryptosQuery } = cryptoApi;
-// console.log('usegetcryptoquery : ',useGetCryptosQuery )
+
+// èèèèèèèèèèèèèèèèèèèèèèèèèèèè
+export const cryptoApi = createApi({
+    reducerPath: 'cryptoApi',
+    baseQuery: fetchBaseQuery({ baseUrl }),
+    endpoints: (builder) => ({
+      getCryptos: builder.query({
+        query: (count) => createRequest(`/coins?limit=${count}`),
+      }),
+// èèèèèèèèèèèèèèèèèèèèèèèèèèèè
+   
+})
+
+})
+export const { useGetCryptosQuery } = cryptoApi;
 
 
 
 
-var axios = require("axios").default;
+// var axios = require("axios").default;
 
-var options = {
-  method: 'GET',
-  url: 'https://coinranking1.p.rapidapi.com/coins',
-//   params: {
-//     referenceCurrencyUuid: 'yhjMzLPhuIDl',
-//     timePeriod: '24h',
-//     symbols: 'BTC',
-//     uuids: 'Qwsogvtv82FCd',
-//     tiers: '1',
-//     tags: 'defi',
-//     orderBy: 'marketCap',
-//     search: 'Bitco',
-//     orderDirection: 'desc',
-//     limit: '50',
-//     offset: '0'
-//   },
-  headers: {
-    'x-rapidapi-host': 'coinranking1.p.rapidapi.com',
-    'x-rapidapi-key': 'ee275a60bcmsh02e88951a12e383p1ee182jsn0a5b1b218f2c'
-  }
-};
+// var options = {
+//   method: 'GET',
+//   url: 'https://coinranking1.p.rapidapi.com/coins',
+// //   params: {
+// //     referenceCurrencyUuid: 'yhjMzLPhuIDl',
+// //     timePeriod: '24h',
+// //     symbols: 'BTC',
+// //     uuids: 'Qwsogvtv82FCd',
+// //     tiers: '1',
+// //     tags: 'defi',
+// //     orderBy: 'marketCap',
+// //     search: 'Bitco',
+// //     orderDirection: 'desc',
+// //     limit: '50',
+// //     offset: '0'
+// //   },
+//   headers: {
+//     'x-rapidapi-host': 'coinranking1.p.rapidapi.com',
+//     'x-rapidapi-key': 'ee275a60bcmsh02e88951a12e383p1ee182jsn0a5b1b218f2c'
+//   }
+// };
 
-axios.request(options).then(function (response) {
-	console.log("LA REPONSE :",response.data);
-}).catch(function (error) {
-	console.error("erreur au niveau du request ",error);
-});
+// axios.request(options).then(function (response) {
+// 	console.log("LA REPONSE :",response.data);
+// }).catch(function (error) {
+// 	console.error("erreur au niveau du request ",error);
+// });
